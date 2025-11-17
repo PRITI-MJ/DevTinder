2f48e940673ce3722d948eddd1c2d5249f983648
Priti <pritimukherjee650@gmail.com>
2025-11-17 23:57:32 +0530

    creating connection Send Requests Models

--- src/routes/request.js
+++ src/routes/request.js
@@ -18,6 +18,34 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       return res.status(400).json({message: "Invalid status type:" + status})
     }
 
+    //checking whether toUserId is present or not
+    const toUser = await UserActivation.findOne(toUserId);
+      if(!toUser){
+        return res.status(404).json({
+          message: "User not found",
+        })
+      }
+
+
+    // Prevent sending connection request to yourself
+      if (String(fromUserId) === String(toUserId)) {
+        return res.status(400).send({ message: "You cannot send a request to yourself." });
+      }
+
+    //IF there is an existing ConnectionRequest. these two things should not be there:-
+    //1. the fromUserId should not send same request to toUserId more than 1 time.(duplicate connection request)
+    //2. the toUserId should not send the connection request to the same fromUserId
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+          {fromUserId, toUserId},
+          { fromUserId: toUserId, toUserId: fromUserId}
+
+        ]
+    })
+
+    if(existingConnectionRequest) {
+      return res.status(400).send({message: "Connection Request Already Exists!!"})
+    }
 
     const connectionRequest = new ConnectionRequest({
       fromUserId, 
