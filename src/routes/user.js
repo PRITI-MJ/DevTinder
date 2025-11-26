e03fd016172f231e2b583a306a5c2c63222fd8f1
Priti <pritimukherjee650@gmail.com>
2025-11-26 23:55:01 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -114,7 +114,7 @@ userRouter.get("/feed", userAuth, async(req, res) => {
         ]
         }).select(USER_SAFE_DATA).skip(skip).limit(limit)
 
-        res.send(feedUsers);
+        res.json({data: feedUsers});
     } 
     catch(err) {
         res.status(400).send("ERROR: " + err.message)
